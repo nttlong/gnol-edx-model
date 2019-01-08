b6a7037e6c0fb6305f6cc0e187c4cc18801fee26
nttlong <zugeliang2000@gamil.com>
2019-01-08 18:11:47 +0700

    save

--- models/course_authors.py
+++ models/course_authors.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from django.contrib.auth.models import User
 class course_authors(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
     # department = models.CharField(max_length=100)
-    user_id =  models.IntegerField()
-    course_id = models.CharField(max_length=255)
+    # user_id =  models.IntegerField()
+    user = models.ForeignKey(User)
+    course = models.ForeignKey(CourseOverview)
+    # course_id = models.CharField(max_length=255)
     created_on =models.DateField()
     id=models.IntegerField()
     class Meta:
